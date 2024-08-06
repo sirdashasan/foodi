e03b72de1f60d837c680da67126f9f0fe10b128c
sirdashasan <sirdashasan@hotmail.com>
2024-08-06 10:24:22 +0300

    menu shop section filter eklendi

--- src/pages/shop/Menu.jsx
+++ src/pages/shop/Menu.jsx
@@ -1,6 +1,75 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Cards from "../../components/Cards";
 
 const Menu = () => {
+  const [menu, setMenu] = useState([]);
+  const [filteredItems, setFilteredItems] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("all");
+  const [sortOption, setSortOption] = useState("default");
+
+  //loading data
+  useEffect(() => {
+    //fetch data from the backend
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/menu.json");
+        const data = await response.json();
+        //console.log(data);
+        setMenu(data);
+        setFilteredItems(data);
+      } catch (error) {
+        console.log("Error fetching data", error);
+      }
+    };
+
+    //call the function
+    fetchData();
+  }, []);
+
+  //filtering data based on category
+  const filterItems = (category) => {
+    const filtered =
+      category === "all"
+        ? menu
+        : menu.filter((item) => item.category === category);
+
+    setFilteredItems(filtered);
+    setSelectedCategory(category);
+  };
+
+  //show all data function
+  const showAll = () => {
+    setFilteredItems(menu);
+    setSelectedCategory("all");
+  };
+
+  //sorting based one A-Z, Z-A, Low-High Pricing
+  const handleSortChange = (option) => {
+    setSortOption(option);
+
+    let sortedItems = [...filteredItems];
+
+    //logic
+    switch (option) {
+      case "A-Z":
+        sortedItems.sort((a, b) => a.name.localeCompare(b.name));
+        break;
+      case "Z-A":
+        sortedItems.sort((a, b) => b.name.localeCompare(a.name));
+        break;
+      case "low-to-high":
+        sortedItems.sort((a, b) => a.price - b.price);
+        break;
+      case "high-to-low":
+        sortedItems.sort((a, b) => b.price - a.price);
+        break;
+      default:
+        break;
+    }
+
+    setFilteredItems(sortedItems);
+  };
+
   return (
     <div>
       {/* menu banner */}
@@ -25,6 +94,57 @@ const Menu = () => {
       </div>
 
       {/* menu shop section */}
+      <div className="section-container">
+        {/* filtering and sorting */}
+        <div>
+          {/* all category btns */}
+          <div className="flex flex-row justify-start md:items-center md:gap-8 gap-4 flex-wrap">
+            <button
+              onClick={showAll}
+              className={selectedCategory === "all" ? "active" : ""}
+            >
+              All
+            </button>
+            <button
+              onClick={() => filterItems("salad")}
+              className={selectedCategory === "salad" ? "active" : ""}
+            >
+              Salad
+            </button>
+            <button
+              onClick={() => filterItems("pizza")}
+              className={selectedCategory === "pizza" ? "active" : ""}
+            >
+              Pizza
+            </button>
+            <button
+              onClick={() => filterItems("soup")}
+              className={selectedCategory === "soup" ? "active" : ""}
+            >
+              Soups
+            </button>
+            <button
+              onClick={() => filterItems("dessert")}
+              className={selectedCategory === "dessert" ? "active" : ""}
+            >
+              Desserts
+            </button>
+            <button
+              onClick={() => filterItems("drinks")}
+              className={selectedCategory === "drinks" ? "active" : ""}
+            >
+              Drinks
+            </button>
+          </div>
+        </div>
+
+        {/* products card */}
+        <div className="grid md:grid-cols-4 sm:grid-cols-2 grid-cols-1 gap-4">
+          {filteredItems.map((item) => (
+            <Cards key={item._id} item={item} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
