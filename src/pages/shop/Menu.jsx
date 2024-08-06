285077d786a912daee5436f895213ef835f38bb2
sirdashasan <sirdashasan@hotmail.com>
2024-08-06 14:51:50 +0300

    menu shop section sorting eklendi

--- src/pages/shop/Menu.jsx
+++ src/pages/shop/Menu.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Cards from "../../components/Cards";
+import { FaFilter } from "react-icons/fa";
 
 const Menu = () => {
   const [menu, setMenu] = useState([]);
@@ -96,7 +97,7 @@ const Menu = () => {
       {/* menu shop section */}
       <div className="section-container">
         {/* filtering and sorting */}
-        <div>
+        <div className="flex flex-col md:flex-row flex-wrap md:justify-between items-center space-y-3 mb-8">
           {/* all category btns */}
           <div className="flex flex-row justify-start md:items-center md:gap-8 gap-4 flex-wrap">
             <button
@@ -135,6 +136,28 @@ const Menu = () => {
             >
               Drinks
             </button>
+          </div>
+
+          {/* sorting base filtering */}
+          <div className="flex justify-end mb-4 rounded-sm">
+            <div className="bg-black p-2">
+              <FaFilter className="h-4 w-4 text-white " />
+            </div>
+
+            {/* sorting options */}
+            <select
+              name="sort"
+              id="sort"
+              onChange={(e) => handleSortChange(e.target.value)}
+              value={sortOption}
+              className="bg-black text-white px-2 py-1 rounded-sm"
+            >
+              <option value="default">Default</option>
+              <option value="A-Z">A-Z</option>
+              <option value="Z-A">Z-A</option>
+              <option value="low-to-high">Low to High</option>
+              <option value="high-to-low">High to Low</option>
+            </select>
           </div>
         </div>
 
