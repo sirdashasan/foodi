04dbe26289cd468420b32d75b0fd04f04d1dfd81
sirdashasan <sirdashasan@hotmail.com>
2024-08-16 15:07:19 +0300

    useCart hooku oluşturuldu

--- src/pages/shop/CartPage.jsx
+++ src/pages/shop/CartPage.jsx
@@ -1,6 +1,32 @@
 import React from "react";
+import useCart from "../../hooks/useCart";
+import { FaTrash } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 const CartPage = () => {
+  const [cart, refatch] = useCart();
+
+  // handledelete btn
+  const handleDelete = (item) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: "Deleted!",
+          text: "Your file has been deleted.",
+          icon: "success",
+        });
+      }
+    });
+  };
+
   return (
     <div className="section-container ">
       {/* banner */}
@@ -32,36 +58,31 @@ const CartPage = () => {
             </thead>
             <tbody>
               {/* row 1 */}
-              <tr>
-                <td>1</td>
-                <td>
-                  <div className="flex items-center gap-3">
-                    <div className="avatar">
-                      <div className="mask mask-squircle h-12 w-12">
-                        <img
-                          src="https://img.daisyui.com/images/profile/demo/2@94.webp"
-                          alt="Avatar Tailwind CSS Component"
-                        />
+              {cart.map((item, index) => (
+                <tr key={index}>
+                  <td>{index + 1}</td>
+                  <td>
+                    <div className="flex items-center gap-3">
+                      <div className="avatar">
+                        <div className="mask mask-squircle h-12 w-12">
+                          <img src={item.image} alt="" />
+                        </div>
                       </div>
                     </div>
-                    <div>
-                      <div className="font-bold">Hart Hagerty</div>
-                      <div className="text-sm opacity-50">United States</div>
-                    </div>
-                  </div>
-                </td>
-                <td>
-                  Zemlak, Daniel and Leannon
-                  <br />
-                  <span className="badge badge-ghost badge-sm">
-                    Desktop Support Technician
-                  </span>
-                </td>
-                <td>Purple</td>
-                <th>
-                  <button className="btn btn-ghost btn-xs">details</button>
-                </th>
-              </tr>
+                  </td>
+                  <td className="font-medium">{item.name}</td>
+                  <td>{item.quantity}</td>
+                  <td>{item.price}</td>
+                  <th>
+                    <button
+                      className="btn btn-ghost text-red btn-xs"
+                      onClick={() => handleDelete(item)}
+                    >
+                      <FaTrash />
+                    </button>
+                  </th>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
