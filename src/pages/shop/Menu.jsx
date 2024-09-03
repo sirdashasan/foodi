07fc0ba9f6d8b55f57fa2366077f7b03caae1cd1
sirdashasan <sirdashasan@hotmail.com>
2024-09-03 12:27:13 +0300

    vercel deneme 4

--- src/pages/shop/Menu.jsx
+++ src/pages/shop/Menu.jsx
@@ -10,8 +10,7 @@ const Menu = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const [itemsPerPage] = useState(8);
 
-  const apiUrl =
-    import.meta.env.VITE_API_URL || "https://foodi-server.vercel.app";
+  const apiUrl = import.meta.env.VITE_API_URL;
 
   //loading data
   useEffect(() => {
