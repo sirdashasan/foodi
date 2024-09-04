9cdfb32659aebc1b46bcec1b72724b9fa57869ec
sirdashasan <sirdashasan@hotmail.com>
2024-09-04 09:48:21 +0300

    CartPage vercel deneme

--- src/pages/shop/CartPage.jsx
+++ src/pages/shop/CartPage.jsx
@@ -92,13 +92,10 @@ const CartPage = () => {
       confirmButtonText: "Yes, delete it!",
     }).then((result) => {
       if (result.isConfirmed) {
-        fetch(
-          `http://localhost:6001/carts/${item._id}`,
-          //fetch(`https://foodi-server-lime.vercel.app/carts/${item._id}`,
-          {
-            method: "DELETE",
-          }
-        )
+        //fetch(`http://localhost:6001/carts/${item._id}`,
+        fetch(`https://foodi-server-lime.vercel.app/carts/${item._id}`, {
+          method: "DELETE",
+        })
           .then((res) => res.json())
           .then((data) => {
             if (data.deletedCount > 0) {
