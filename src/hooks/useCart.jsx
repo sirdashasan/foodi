0a27c4b8c23511ee98ecb7126c7a3090f8cec532
sirdashasan <sirdashasan@hotmail.com>
2024-09-04 09:51:39 +0300

    useCart vercel deneme

--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -7,10 +7,10 @@ const useCart = () => {
   const { refetch, data: cart = [] } = useQuery({
     queryKey: ["carts", user?.email],
     queryFn: async () => {
+      //const res = await fetch(`http://localhost:6001/carts?email=${user?.email}`);
       const res = await fetch(
-        `http://localhost:6001/carts?email=${user?.email}`
+        `https://foodi-server-lime.vercel.app/carts?email=${user?.email}`
       );
-      //const res = await fetch(`https://foodi-server-lime.vercel.app/carts?email=${user?.email}`);
       return res.json();
     },
   });
