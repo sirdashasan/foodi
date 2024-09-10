6cc4a0d27a61e2143c7021933a4aa5f08b02d0f1
sirdashasan <sirdashasan@hotmail.com>
2024-09-10 10:58:50 +0300

    local storage eklendi

--- src/contexts/AuthProvider.jsx
+++ src/contexts/AuthProvider.jsx
@@ -10,6 +10,7 @@ import {
   updateProfile,
 } from "firebase/auth";
 import app from "../firebase/firebase.config";
+import axios from "axios";
 
 export const AuthContext = createContext();
 const auth = getAuth(app);
@@ -51,6 +52,17 @@ const AuthProvider = ({ children }) => {
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
+      if (currentUser) {
+        const userInfo = { email: currentUser.email };
+        axios.post("http://localhost:6001/jwt", userInfo).then((response) => {
+          //console.log(response.data.token);
+          if (response.data.token)
+            localStorage.setItem("access-token", response.data.token);
+        });
+      } else {
+        localStorage.removeItem("access-token");
+      }
+
       setLoading(false);
     });
 
