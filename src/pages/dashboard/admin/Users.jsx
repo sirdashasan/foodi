e5f4a68c1f2d562b0087e3f81f9d9c3860db0a31
sirdashasan <sirdashasan@hotmail.com>
2024-09-10 16:38:02 +0300

    Dashboard admin kontrolü eklendi

--- src/pages/dashboard/admin/Users.jsx
+++ src/pages/dashboard/admin/Users.jsx
@@ -1,7 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import React, { useState } from "react";
 import { FaTrashAlt, FaUser, FaUsers } from "react-icons/fa";
-import Login from "../../../components/Login";
 
 const Users = () => {
   const { refetch, data: users = [] } = useQuery({
@@ -12,63 +11,55 @@ const Users = () => {
     },
   });
 
-  const isAdmin = false;
-
   console.log(users);
   return (
     <div>
-      {isAdmin ? (
-        <div>
-          <div className="flex items-center justify-between m-4">
-            <h5>All Users</h5>
-            <h5>Total Users: {users.length}</h5>
-          </div>
+      <div className="flex items-center justify-between m-4">
+        <h5>All Users</h5>
+        <h5>Total Users: {users.length}</h5>
+      </div>
 
-          {/* table */}
-          <div>
-            <div className="overflow-x-auto">
-              <table className="table table-zebra md:w-[870px]">
-                {/* head */}
-                <thead className="bg-green text-white rounded-lg">
-                  <tr>
-                    <th>#</th>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Role</th>
-                    <th>Action</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {/* row 1 */}
-                  {users.map((user, index) => (
-                    <tr key={index}>
-                      <th>{index + 1}</th>
-                      <td>{user.name}</td>
-                      <td>{user.email}</td>
-                      <td>
-                        {user.role === "admin" ? (
-                          "Admin"
-                        ) : (
-                          <button className="btn btn-xs btn-circle bg-indigo-500 text-white">
-                            <FaUsers />
-                          </button>
-                        )}
-                      </td>
-                      <td>
-                        <button className="btn btn-xs bg-orange-500 text-white">
-                          <FaTrashAlt />
-                        </button>
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
-          </div>
+      {/* table */}
+      <div>
+        <div className="overflow-x-auto">
+          <table className="table table-zebra md:w-[870px]">
+            {/* head */}
+            <thead className="bg-green text-white rounded-lg">
+              <tr>
+                <th>#</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Role</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* row 1 */}
+              {users.map((user, index) => (
+                <tr key={index}>
+                  <th>{index + 1}</th>
+                  <td>{user.name}</td>
+                  <td>{user.email}</td>
+                  <td>
+                    {user.role === "admin" ? (
+                      "Admin"
+                    ) : (
+                      <button className="btn btn-xs btn-circle bg-indigo-500 text-white">
+                        <FaUsers />
+                      </button>
+                    )}
+                  </td>
+                  <td>
+                    <button className="btn btn-xs bg-orange-500 text-white">
+                      <FaTrashAlt />
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
-      ) : (
-        <Login />
-      )}
+      </div>
     </div>
   );
 };
