bad134c4681e8b7c381ecaad5e394094b4555c8c
sirdashasan <sirdashasan@hotmail.com>
2024-09-12 13:57:15 +0300

    AddMenu güncellendi image hosting eklendi

--- src/pages/dashboard/admin/AddMenu.jsx
+++ src/pages/dashboard/admin/AddMenu.jsx
@@ -1,11 +1,50 @@
 import React from "react";
 import { FaUtensils } from "react-icons/fa";
 import { useForm } from "react-hook-form";
+import useAxiosPublic from "../../../hooks/useAxiosPublic";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import Swal from "sweetalert2";
 
 const AddMenu = () => {
-  const { register, handleSubmit } = useForm();
-  const onSubmit = (data) => {
-    console.log(data);
+  const { register, handleSubmit, reset } = useForm();
+  const axiosPublic = useAxiosPublic();
+  const axiosSecure = useAxiosSecure();
+
+  // image hosting key
+  const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY;
+  //console.log(image_hosting_key);
+  const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`;
+
+  const onSubmit = async (data) => {
+    //console.log(data);
+    const imageFile = { image: data.image[0] };
+    const hostingImg = await axiosPublic.post(image_hosting_api, imageFile, {
+      headers: {
+        "content-type": "multipart/form-data",
+      },
+    });
+    //console.log(hostingImg);
+    if (hostingImg.data.success) {
+      const menuItem = {
+        name: data.name,
+        category: data.category,
+        price: parseFloat(data.price),
+        recipe: data.recipe,
+        image: hostingImg.data.data.display_url,
+      };
+      //console.log(menuItem);
+      const postMenuItem = axiosSecure.post("/menu", menuItem);
+      if (postMenuItem) {
+        reset();
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: "Your Item is inserted successfully!",
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      }
+    }
   };
 
   return (
