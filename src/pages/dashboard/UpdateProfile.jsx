2a1d0ee1cd190317d2600e9a75950b6ae0020830
sirdashasan <sirdashasan@hotmail.com>
2024-09-25 15:37:04 +0300

    UpdateProfile güncellendi

--- src/pages/dashboard/UpdateProfile.jsx
+++ src/pages/dashboard/UpdateProfile.jsx
@@ -4,46 +4,38 @@ import { AuthContext } from "../../contexts/AuthProvider";
 import { useLocation, useNavigate } from "react-router-dom";
 
 const UpdateProfile = () => {
-  const { updateuserProfile } = useContext(AuthContext);
-
+  const { updateUserProfile } = useContext(AuthContext);
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
-
-  const location = useLocation();
-  const navigate = useNavigate();
-  const from = location.state?.from?.pathname || "/";
-
   const onSubmit = (data) => {
     const name = data.name;
     const photoURL = data.photoURL;
-    updateuserProfile(name, photoURL)
+
+    updateUserProfile(name, photoURL)
       .then(() => {
         // Profile updated!
-        alert("Profile updated successfully!");
-        navigate(from, { replace: true });
+        alert("Profile updated successfully");
       })
       .catch((error) => {
         // An error occurred
-        console.error("Error updating profile: ", error);
+        // ...
       });
   };
-
   return (
-    <div className="flex items-center justify-center h-screen">
-      <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+    <div className="h-screen max-w-md mx-auto flex items-center justify-center ">
+      <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
         <form className="card-body" onSubmit={handleSubmit(onSubmit)}>
-          <h3 className="font-bold">Update Your Profile</h3>
           <div className="form-control">
             <label className="label">
               <span className="label-text">Name</span>
             </label>
             <input
-              {...register("name")}
               type="text"
-              placeholder="your name"
+              {...register("name")}
+              placeholder="Your name"
               className="input input-bordered"
               required
             />
@@ -52,20 +44,19 @@ const UpdateProfile = () => {
             <label className="label">
               <span className="label-text">Upload Photo</span>
             </label>
-
             <input
+              type="file"
               {...register("photoURL")}
-              type="text"
-              placeholder="photoURL"
-              className="input input-bordered"
-              required
+              className="file-input w-full mt-1"
             />
-
-            {/* TODO: Uploading image will be later */}
-            {/* <input type="file" className="file-input w-full max-w-xs" /> */}
+            {/* <input type="text" {...register("photoURL")} placeholder="photo url" className="input input-bordered" required /> */}
           </div>
           <div className="form-control mt-6">
-            <button className="btn bg-green text-white">Update</button>
+            <input
+              type="submit"
+              value={"Update"}
+              className="btn bg-green text-white"
+            />
           </div>
         </form>
       </div>
